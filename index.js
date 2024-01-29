8cd4264dafec3c47c4d5897ad659408a2741458e
pramudithaSJ <IT20620202@my.sliit.lk>
2024-01-30 00:18:29 +0530

    fixed bulid issue

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const responseEnhancer = require('express-response-formatter').responseEnhancer
 
 dotenv.config();
 
@@ -17,7 +16,6 @@ app.use(cors({
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(responseEnhancer())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
