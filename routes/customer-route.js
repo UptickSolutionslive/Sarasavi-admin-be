99db84bbb6df13fd689d63b85720b8e0cf827d93
Kushan Migara Ranasinghe <it20600884@my.sliit.lk>
2024-01-30 11:49:24 +0530

    edit routes

--- routes/customer-route.js
+++ routes/customer-route.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-let User = require("../models/customer-model")
-
 const controller = require("../controllers/customer-controller");
 
 
@@ -23,16 +21,7 @@ router.route("/:id").delete((req, res) => {
 })
 
 
-//get one of the user
-//http://localhost:3000/user/get/:id
-router.route("/get/:id").get((req, res) => {
-    let id = req.params.id;
-    User.findById(id).then((user) => {
-        res.json(user)
-    }).catch((err) => {
-        console.log(err);
-    })
-})
+
 
 
 module.exports = router;
