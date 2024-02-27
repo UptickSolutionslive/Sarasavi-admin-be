1016d34b49039def2bb2683daf4b3421dbb0d2cf
pramudithaSJ <IT20620202@my.sliit.lk>
2024-02-27 08:50:09 +0530

    receipt

---
+++ models/cheque-model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chequeSchema = new Schema({
+    cheque_no: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    bank: {
+        type: String,
+        required: true
+    },
+    customer: {
+        type: Schema.Types.ObjectId,
+        ref: 'customer',
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    remarks: {
+        type: String,
+    },
+
+
+}, { timestamps: true })
+module.exports = mongoose.model("cheque", chequeSchema);
