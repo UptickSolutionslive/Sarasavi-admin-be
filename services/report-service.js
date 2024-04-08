a72b6a6d8e30013fa89eb9d23786c6f551d0ab5c
Pramuditha Jayawardhana <pramusj@Pramudithas-MacBook-Air-2.local>
2024-04-08 13:36:35 +0530

    report

--- services/report-service.js
+++ services/report-service.js
@@ -3,59 +3,40 @@ const Order = require("../models/order-model");
 
 async function getInvoiceByCuzAndDate(data) {
   try {
-    console.log(data);
-    const cuzId = data.customerId;
-    const dateType = data.dateType;
-    const date = data.date;
-    const startDate = data.startDate;
-    const endDate = data.endDate;
-    const month = data.month;
-    const invoices = [];
+    const { customerId, dateType, date, startDate, endDate, month } = data;
+    let ordersQuery = { "customer.customerId": customerId };
+    let invoices = [];
 
-    if (dateType === "daily") {
-      const orders = await Order.find({
-        "customer.customerId": cuzId,
-        date: date,
-      });
-      for (let i = 0; i < orders.length; i++) {
-        const invoice = await Invoice.find({
-          order_id: orders[i]._id,
-        });
-        invoices.push(invoice);
-      }
-    }
-    if (dateType === "monthly") {
-      const orders = await Order.find({
-        "customer.customerId": cuzId,
-        date: { $regex: month },
-      });
-      for (let i = 0; i < orders.length; i++) {
-        const invoice = await Invoice.find({
-          order_id: orders[i]._id,
-        });
-        invoices.push(invoice);
-      }
-    }
-    if (dateType === "range") {
-      console.log("here");
+    switch (dateType) {
+      case "daily":
+        const startOftheDay = new Date(date);
+        startOftheDay.setHours(0, 0, 0, 0);
+        const endOftheDay = new Date(date);
+        endOftheDay.setHours(23, 59, 59, 999);
+        ordersQuery.date = { $gte: startOftheDay, $lte: endOftheDay };
+        break;
+      case "monthly":
+        const year = parseInt(month.substring(0, 4));
+        const monthNumber = parseInt(month.substring(5)) - 1; // Months are 0-indexed
+        const startOfMonth = new Date(year, monthNumber, 1); // Start of the month
+        const endOfMonth = new Date(year, monthNumber + 1, 0); // End of the month
+        ordersQuery.date = { $gte: startOfMonth, $lt: endOfMonth };
 
-      const orders = await Order.find({
-        "customer.customerId": cuzId,
-        date: { $gte: startDate, $lte: endDate },
-      });
-      for (let i = 0; i < orders.length; i++) {
-        const invoice = await Invoice.find({
-          order_id: orders[i]._id,
-        });
-        invoices.push(invoice);
-      }
+        break;
+      case "range":
+        ordersQuery.date = { $gte: startDate, $lte: endDate };
+        break;
+      default:
+        throw new Error("Invalid dateType");
     }
-    console.log(invoices);
+
+    const orders = await Order.find(ordersQuery);
+
+    for (let i = 0; i < orders.length; i++) {
+      const invoice = await Invoice.find({ order_id: orders[i]._id });
+      invoices = invoices.concat(invoice); // Flatten and concatenate
+    }
     return { status: 200, invoices };
-    // const invoices = await Invoice.find({
-    //   order_id: { $in: orders.map((order) => order._id) },
-    // })
-    // return invoices;
   } catch (error) {
     console.log(error);
     throw new Error(error);
