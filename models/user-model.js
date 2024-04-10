aa47e6395749bb3064bde7ad5b4f402114cb5abf
Desh1100 <deshansameera1100@gmail.com>
2024-04-10 11:59:01 +0530

    user model modified

--- models/user-model.js
+++ models/user-model.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
-    username: {
-        type: String,
-        required: true,
-        unique:true,
-    },
-    password: {
-        type: String,
-        required:true
-    },
-    role: {
-        type: String,
-        required:true
-    }
-
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
 module.exports = mongoose.model("user", userSchema);
