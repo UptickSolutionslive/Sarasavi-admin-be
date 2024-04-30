e0c131406273fc7bbf3e676847d3998d31c5c157
Pramuditha Jayawardhana <pramusj@Pramudithas-MacBook-Air-2.local>
2024-04-30 10:02:55 +0530

    route crud

---
+++ models/route-model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const routeSchema = new Schema({
+
+    name: {
+        type: String,
+        required: true,
+        unique:true,
+    },
+    description:{
+        type: String,
+        required:true,
+    },
+    created_at:{
+        type: Date,
+        default: Date.now
+    },
+    
+
+})
+module.exports = mongoose.model("route", routeSchema);
